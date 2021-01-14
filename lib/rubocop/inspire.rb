67f10c3380f52b2450a417fc0c94fb47c125ce3d
Jay Dorsey <jaydorsey@fastmail.com>
2021-01-14 12:51:06 -0500

    First commit
        bundle gem rubocop-inspire

---
+++ lib/rubocop/inspire.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "inspire/version"
+
+module Rubocop
+  module Inspire
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
